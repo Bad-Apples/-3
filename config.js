abb8f1bf95d3cc3643ff1dd3aa7fa41fbbf48f54
VinhIT <vinhphanthe279@gmail.com>
2021-11-12 22:58:45 +0700

    <3

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo Mẩu! 🖐🖐🖐',
+    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Em yêu anh nhất phải hông ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là thừa nhận đó nha :> ',
+    btnYes: 'Có, em thật sự rất rất yêu anh 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gửi ! 💌💌',
+    reply: 'Tại vì Vinh đẹp trai đó hihi 😊😊',
+    mess: 'Anh biết mà, Anh cũng yêu em lắm 🥰. Love you 3000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.messenger.com/t/100008889455156'
+}
